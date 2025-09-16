511bf817ca5548092d2c066cdf62cacd014da874
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-15 22:07:31 -0300

    first comm

---
+++ app/src/main/java/com/example/fundamentos_jetpack_compose_listas_lazy/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.fundamentos_jetpack_compose_listas_lazy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.fundamentos_jetpack_compose_listas_lazy.components.GameCard
+import com.example.fundamentos_jetpack_compose_listas_lazy.repository.getAllGames
+import com.example.fundamentos_jetpack_compose_listas_lazy.ui.theme.FundamentosjetpackcomposelistaslazyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FundamentosjetpackcomposelistaslazyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GamesScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+
+fun GamesScreen(modifier: Modifier) {
+
+    val searchTextState by remember { mutableStateOf("") }
+    val gamesListState by remember { mutableStateOf(getAllGames()) }
+
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "Meus jogos favoritos",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text(text = "Nome do estúdio") },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = ""
+                    )
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn() {
+            items(getAllGames()) {
+                GameCard(game = it)
+            }
+        }
+    }
+}
+
+
